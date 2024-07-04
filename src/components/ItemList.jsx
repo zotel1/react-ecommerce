0c06aa12579e9a926c5839c071524e7ce268d6f5
zotel1 <zotelsigel@gmail.com>
2024-07-04 16:00:55 -0300

    update: con la funcion toCapital hacemos que cada titulo de nuestra categoria comience con una letra en mayuscula

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,10 @@
+import { toCapital } from "../helpers/toCapital";
 import Item from "./Item"
 
 const ItemList = ( {productos, titulo} ) => {
     return (
         <div className="container">
-            <h2 className="main-title">{titulo}</h2>
+            <h2 className="main-title">{toCapital(titulo)}</h2>
             <div>
                 { productos.map((prod) => <Item key={prod.id} producto={prod} />) }
             </div>
