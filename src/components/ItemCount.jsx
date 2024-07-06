0edb9a1f80cf0d26376753f2cea70b1d44094ba2
zotel1 <zotelsigel@gmail.com>
2024-07-06 17:30:30 -0300

    update

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
-const ItemCount = ( ) => {
+const ItemCount = ( {item} ) => {
 
     const [cantidad, setCantidad] = useState(1);
+
+    const handleRestar = () => {
+        cantidad > 1 && setCantidad(cantidad -1)
+    }
+
+    const handleSumar = () => {
+        cantidad < item.stock && setCantidad(cantidad + 1)
+    }
+
     return ( 
         <div>
             <div className="item-count">
-                <button>-</button>
+                <button onClick={handleRestar}>-</button>
                 <p>{cantidad}</p>
-                <button>+</button>
+                <button onClick={handleSumar}>+</button>
             </div>
             <button className="agregar-al-carrito">Agregar al carrito</button>
         </div>
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,17 +1,19 @@
 import {toCapital} from "../helpers/toCapital"
 import ItemCount from "./ItemCount";
+
 const ItemDetail = ( {item} ) => {
     return (
         <div className="container">
-            <img src={item.imagen} alt={item.titulo}/>
+            <div className="producto-detalle">
+                <img src={item.imagen} alt={item.titulo} />
             <div>
                 <h3 className="titulo">{item.titulo}</h3>
                 <p className="descripcion">{item.descripcion}</p>
                 <p className="categoria">Categoría: {toCapital(item.categoria)}</p>
                 <p className="precio">${item.precio}</p>
-                <ItemCount />
+                <ItemCount item={item} />
             </div>
-
+            </div>
         </div>
     )
 };
