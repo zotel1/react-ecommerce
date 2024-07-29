a894ce61eb81227d08ec515c33b897cecf46e34a
zotel1 <zotelsigel@gmail.com>
2024-07-29 02:49:48 -0300

    cambiamos las media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
         <nav className="navbar">
             <Link to="/" className="logo"><h1>Mundo limpio</h1></Link>
             <ul className="menu">
-                <li><Link className="menu-link" to="/">Inicio</Link></li>
                 <li><Link className="menu-link" to="/productos">Productos</Link></li>
                 <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
                 <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
